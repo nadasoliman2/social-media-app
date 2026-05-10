afc601a1242650343be260b0bf18b25964e22ba7
nadasoliman2 <nadaemad271@gmail.com>
2026-05-11 00:12:22 +0300

    updates

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
-import app from "../app.bootstrap.js";
-import { connectDB, redisconnection } from "../DB/index.js"; // تأكدي من المسار
-
+import app from "../src/app.bootstrap.js";
+import { connectDB, redisconnection } from "../src/DB/index.js"; // تأكدي من المسار
 export default async function handler(req, res) {
     try {
         // تأكدي إن connectDB جواها check لو فيه connection فعلاً
         await connectDB();
         await redisconnection();
-        
         return app(req, res);
-    } catch (error) {
+    }
+    catch (error) {
         console.error("Database connection error:", error);
         res.status(500).json({ error: "Internal Server Error", message: error.message });
     }
